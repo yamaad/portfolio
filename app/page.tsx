2d23e293eab4ab44a469447a4b4da06ce7933953
yamaad <maad@quickdesk.io>
2024-08-04 17:56:55 +0800

    hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,34 @@
+"use client";
 import Hero from "@/components/Hero";
-import Image from "next/image";
+import { LampContainer } from "@/components/ui/lamp";
+import { Navbar } from "@/components/ui/Navbar";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto">
+    <main className="relative dark:bg-black-100 flex justify-center items-center flex-col mx-auto">
       <div className="max-w-7xl w-full">
+        <LampContainer>
+          <motion.h1
+            initial={{ opacity: 0, y: -150 }}
+            whileInView={{ opacity: 1, y: 10 }}
+            transition={{
+              delay: 0.3,
+              duration: 1.5,
+              ease: "easeInOut",
+            }}
+            className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl "
+          >
+            Welcome Aboard!
+          </motion.h1>
+        </LampContainer>
+        <Navbar
+          navItems={[
+            { name: "Top", link: "#" },
+            { name: "Top", link: "#" },
+            { name: "Top", link: "#" },
+          ]}
+        />
         <Hero />
       </div>
     </main>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,31 @@
+"use client";
+
 import React from "react";
-import { LampContainer } from "./ui/lamp";
+import { TextGenerateEffect } from "./ui/text-generate-effect";
+import { BorderMagicButton } from "./ui/BorderMagicButton";
+import { FaLocationArrow } from "react-icons/fa6";
 
 const Hero = () => {
-  return <LampContainer>xxx</LampContainer>;
+  return (
+    <div>
+      <div className="h-screen w-full  dark:bg-grid-orange-200/[0.1] bg-grid-black/[0.1]  flex items-center justify-center absolute top-0 left-06 ">
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_1%,black)] "></div>
+        <div className="flex justify-center relative my-20 ">
+          <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vwh] flex flex-col item-center justify-center ">
+            <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80 ">dynamic web magic with Next.Js</h2>
+            <TextGenerateEffect className="text-center text-[40px] md:text-5xl lg:text-6xl" words="Transforming Concepts into Seamless Experiences" />
+            <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-xxl">
+              Hi I&apos;m Maad, a full stack developer based in Kuala Lumpur
+            </p>
+            <a href="#about">
+              <BorderMagicButton title="show off" icon={<FaLocationArrow />} position="right" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
