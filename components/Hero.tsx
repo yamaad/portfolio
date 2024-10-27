ebd2fb4f059c00023361967c8f35283f504a3c08
yamaad <maad@quickdesk.io>
2024-10-28 07:38:32 +0800

    text genrator & magic button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import React from "react";
-import { TextGenerateEffect } from "./ui/text-generate-effect";
-import { BorderMagicButton } from "./ui/BorderMagicButton";
-import { FaLocationArrow } from "react-icons/fa6";
+import { TextGenerateEffect } from "./ui/textGenerateEffect";
 import { Spotlight } from "./ui/spotlight";
+import { BorderMagicButton } from "./ui/BorderMagicButton";
+import { FaLocationArrow } from "react-icons/fa";
 
 const Hero = () => {
   return (
@@ -12,22 +12,25 @@ const Hero = () => {
       <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
       <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill="purple" />
       <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
-      {/* <div className="h-screen w-full  dark:bg-grid-orange-200/[0.1] bg-grid-black/[0.1]  flex items-center justify-center absolute top-0 left-06 ">
-
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_1%,black)] "></div>
-        <div className="flex justify-center relative my-20 ">
-          <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vwh] flex flex-col item-center justify-center ">
-            <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80 ">dynamic web magic with Next.Js</h2>
-            <TextGenerateEffect className="text-center text-[40px] md:text-5xl lg:text-6xl" words="Transforming Concepts into Seamless Experiences" />
-            <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-xxl">
-              Hi I&apos;m Maad, a full stack developer based in Kuala Lumpur
-            </p>
-            <a href="#about">
-              <BorderMagicButton title="show off" icon={<FaLocationArrow />} position="right" />
-            </a>
-          </div>
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      </div>
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">Dynamic Web Magic with Next.js</h2>
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Transforming  Concepts into  Seamless  User  Experiences."
+          />
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi I'm Maad, a Software Developer based in Kuala Lumpur.
+          </p>
+          <a href="#about">
+            <BorderMagicButton title="Show My Work" icon={<FaLocationArrow />} position="right" />
+          </a>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 };
